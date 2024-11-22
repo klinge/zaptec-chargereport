d308cc1bb890652d99d1b7adc188c60d08c455bf
Johan Klinge <johan.klinge@gmail.com>
2024-11-22 15:04:46 +0100

    Added both invoice report and summary report to main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def main():
     report = MonthlySummaryReport()
     report.generate_report()
 
-    #report = InvoicingReport()
-    #report.generate_report()
+    invoice = InvoicingReport()
+    invoice.generate_report()
 
     #with ZaptecApi() as api:
     #    result = api.get_installation_report("2024-11-01T00:00:00.000", "2024-11-21T23:59:59.999")
